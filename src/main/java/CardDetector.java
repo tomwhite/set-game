f23a8dfc63b31ff460dd11ef66a2cc89ac2b6822
Tom White <tom@cloudera.com>
2016-12-30 13:07:32 +0000

    Try binarization (but with no blurring yet)

--- src/main/java/CardDetector.java
+++ src/main/java/CardDetector.java
@@ -3,6 +3,8 @@ import boofcv.alg.feature.detect.edge.CannyEdge;
 import boofcv.alg.feature.detect.edge.EdgeContour;
 import boofcv.alg.filter.binary.BinaryImageOps;
 import boofcv.alg.filter.binary.Contour;
+import boofcv.alg.filter.binary.GThresholdImageOps;
+import boofcv.alg.filter.binary.ThresholdImageOps;
 import boofcv.alg.filter.blur.BlurImageOps;
 import boofcv.alg.filter.blur.GBlurImageOps;
 import boofcv.factory.feature.detect.edge.FactoryEdgeDetectors;
@@ -13,6 +15,7 @@ import boofcv.gui.image.ShowImages;
 import boofcv.io.image.ConvertBufferedImage;
 import boofcv.io.image.UtilImageIO;
 import boofcv.struct.ConnectRule;
+import boofcv.struct.image.GrayF32;
 import boofcv.struct.image.GrayS16;
 import boofcv.struct.image.GrayS32;
 import boofcv.struct.image.GrayU8;
@@ -101,7 +104,52 @@ public class CardDetector {
 
   }
 
+  public void scan2(String filename) throws IOException {
+    BufferedImage image = UtilImageIO.loadImage(filename);
+
+    // from http://boofcv.org/index.php?title=Example_Binary_Image
+
+    // convert into a usable format
+    GrayF32 input = ConvertBufferedImage.convertFromSingle(image, null, GrayF32.class);
+    GrayU8 binary = new GrayU8(input.width,input.height);
+    GrayS32 label = new GrayS32(input.width,input.height);
+
+    // Select a global threshold using Otsu's method.
+    double threshold = GThresholdImageOps.computeOtsu(input, 0, 255);
+
+    // Apply the threshold to create a binary image
+    ThresholdImageOps.threshold(input, binary, (float) threshold, true);
+
+    // remove small blobs through erosion and dilation
+    // The null in the input indicates that it should internally declare the work image it needs
+    // this is less efficient, but easier to code.
+    GrayU8 filtered = BinaryImageOps.erode8(binary, 1, null);
+    filtered = BinaryImageOps.dilate8(filtered, 1, null);
+
+    // Detect blobs inside the image using an 8-connect rule
+    List<Contour> contours = BinaryImageOps.contour(filtered, ConnectRule.EIGHT, label);
+
+    // colors of contours
+    int colorExternal = 0xFFFFFF;
+    int colorInternal = 0xFF2020;
+
+    // display the results
+    BufferedImage visualBinary = VisualizeBinaryData.renderBinary(binary, false, null);
+    BufferedImage visualFiltered = VisualizeBinaryData.renderBinary(filtered, false, null);
+    BufferedImage visualLabel = VisualizeBinaryData.renderLabeledBG(label, contours.size(), null);
+    BufferedImage visualContour = VisualizeBinaryData.renderContours(contours, colorExternal, colorInternal,
+            input.width, input.height, null);
+
+    ListDisplayPanel panel = new ListDisplayPanel();
+    panel.addImage(visualBinary, "Binary Original");
+    panel.addImage(visualFiltered, "Binary Filtered");
+    panel.addImage(visualLabel, "Labeled Blobs");
+    panel.addImage(visualContour, "Contours");
+    ShowImages.showWindow(panel,"Binary Operations",true);
+
+  }
+
   public static void main(String[] args) throws IOException {
-    new CardDetector().scan(args[0]);
+    new CardDetector().scan2(args[0]);
   }
 }
