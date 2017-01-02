af1c6881c9d56308afd390b86f036f66372712b5
Tom White <tom@cloudera.com>
2017-01-02 18:10:07 +0000

    Add program to create training set

---
+++ src/main/java/com/tom_e_white/set_game/CreateTrainingSet.java
@@ -0,0 +1,32 @@
+package com.tom_e_white.set_game;
+
+import boofcv.io.image.UtilImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateTrainingSet {
+    public static void main(String[] args) {
+        CardDetector cardDetector = new CardDetector();
+        File outDir = new File("data/train-out");
+        outDir.mkdirs();
+        Arrays.stream(new File("data/train").listFiles((dir, name) -> name.matches(".*\\.jpg"))).forEach(
+                file -> {
+                    try {
+                        System.out.println(file);
+                        List<BufferedImage> images = cardDetector.scan(file.getAbsolutePath());
+                        int i = 1;
+                        for (BufferedImage image : images) {
+                            File newFile = new File(outDir, file.getName().replace(".jpg", "_" + i++ + ".jpg"));
+                            UtilImageIO.saveImage(image, newFile.getAbsolutePath());
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+    }
+}
