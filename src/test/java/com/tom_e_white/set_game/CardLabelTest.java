7f20f437a49488601ecf4244d5142a83ca373978
Tom White <tom@cloudera.com>
2017-03-04 15:40:54 +0000

    Add card label tests

--- src/test/java/com/tom_e_white/set_game/CardLabelTest.java
+++ src/test/java/com/tom_e_white/set_game/CardLabelTest.java
@@ -8,8 +8,19 @@ import static org.junit.Assert.assertEquals;
 
 public class CardLabelTest {
     @Test
-    public void testGetColourNumber() {
-        assertEquals(1, CardLabel.getColourNumber("3 filled red squiggles"));
-        assertEquals(1, CardLabel.getColourNumber(new File("data/train-out/red-1-20170101_124752_6.jpg")));
+    public void testGetNumbersForLabel() {
+        String label = "3 filled red squiggles";
+        assertEquals(3, CardLabel.getNumber(label));
+        assertEquals(1, CardLabel.getShadingNumber(label));
+        assertEquals(1, CardLabel.getColourNumber(label));
+        assertEquals(3, CardLabel.getShapeNumber(label));
+    }
+    @Test
+    public void testGetNumbersForFile() {
+        File file = new File("data/train-out/red-1-20170101_124752_6.jpg");
+        assertEquals(1, CardLabel.getNumber(file));
+        assertEquals(2, CardLabel.getShadingNumber(file));
+        assertEquals(1, CardLabel.getColourNumber(file));
+        assertEquals(3, CardLabel.getShapeNumber(file));
     }
 }
