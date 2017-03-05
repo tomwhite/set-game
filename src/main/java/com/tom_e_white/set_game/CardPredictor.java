d63f351f0317df5c7183ff4afcaf5e04ad1375f7
Tom White <tom@cloudera.com>
2017-03-05 17:18:30 +0000

    CardPredictor

---
+++ src/main/java/com/tom_e_white/set_game/CardPredictor.java
@@ -0,0 +1,26 @@
+package com.tom_e_white.set_game;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ * Predict the card that appears in an image using trained models.
+ */
+public class CardPredictor {
+
+    private final FeatureFinder numberFinder = new FindCardNumberFeatures();
+    private final FeatureFinder shadingFinder = new FindCardShadingFeatures();
+    private final FeatureFinder colourFinder = new FindCardColourFeatures();
+    private final FeatureFinder shapeFinder = new FindCardShapeFeatures();
+
+    public Card predict(BufferedImage image) throws IOException, ParseException {
+        return new Card(predict(numberFinder, image), predict(shadingFinder, image),
+                predict(colourFinder, image), predict(shapeFinder, image));
+    }
+
+    private int predict(FeatureFinder finder, BufferedImage image) throws IOException, ParseException {
+        double[] features = finder.find(image, false);
+        return finder.getClassifier().predict(features);
+    }
+}
--- src/main/java/com/tom_e_white/set_game/PredictCardsOnTestData.java
+++ src/main/java/com/tom_e_white/set_game/PredictCardsOnTestData.java
@@ -10,27 +10,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Use feature finders to predict each card in the test set.
+ * Use {@link CardPredictor} to predict each card in the test set.
  */
 public class PredictCardsOnTestData {
 
     public static double predict(File testFile) throws IOException, ParseException {
-
-        FeatureFinder numberFinder = new FindCardNumberFeatures();
-        FeatureFinder shadingFinder = new FindCardShadingFeatures();
-        FeatureFinder colourFinder = new FindCardColourFeatures();
-        FeatureFinder shapeFinder = new FindCardShapeFeatures();
-
         CardDetector cardDetector = new CardDetector();
         List<BufferedImage> images = cardDetector.scan(testFile.getAbsolutePath(), false);
         List<String> testDescriptions = Files.lines(Paths.get(testFile.getAbsolutePath().replace(".jpg", ".txt"))).collect(Collectors.toList());
 
+        CardPredictor cardPredictor = new CardPredictor();
         int correct = 0;
         int total = 0;
         for (int i = 0; i < testDescriptions.size(); i++) {
-            BufferedImage image = images.get(i);
-            Card predictedCard = new Card(predict(numberFinder, image), predict(shadingFinder, image),
-                    predict(colourFinder, image), predict(shapeFinder, image));
+            Card predictedCard = cardPredictor.predict(images.get(i));
             Card actualCard = new Card(testDescriptions.get(i));
             if (predictedCard.equals(actualCard)) {
                 correct++;
@@ -45,11 +38,6 @@ public class PredictCardsOnTestData {
         System.out.println("Accuracy: " + ((int) accuracy) + " percent");
         System.out.println("------------------------------------------");
         return accuracy;
-    }
-
-    private static int predict(FeatureFinder finder, BufferedImage image) throws IOException, ParseException {
-        double[] features = finder.find(image, false);
-        return finder.getClassifier().predict(features);
     }
 
     public static void main(String[] args) throws Exception {
