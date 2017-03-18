5574cfe31b21ab79bed7dfc84f517541524301f4
Tom White <tom@cloudera.com>
2017-03-18 17:57:14 +0000

    Improve training preprocessing pipeline.

--- data/train-v2/README.md
+++ data/train-v2/README.md
@@ -1,10 +1,34 @@
 The _train-v2_ dataset is a collection of Set card images.
 
-The _raw_ directory contains original camera images. Each image is a 
+The _raw-archive_ directory contains original camera images. Each image is a 
 photo of a board of 27 images, arranged in a 3 by 9 grid. There are three
 boards, one contains all the Set cards with one shape, one with two shapes,
 and one with three shapes.
 
-The _raw_ images are photos of the same boards taken at different times under
+The _raw-archive_ images are photos of the same boards taken at different times under
 varying lighting conditions, from different angles, and with different cameras.
+
+### Process
+
+1. Copy photos from the camera to a new _raw-archive/batch-nnnnn_ directory.
+2. Visually inspect the images in Preview and make sure they all oriented correctly.
+(Open the Inspector, and check the Orientation - it should be 1.) Rotate any that
+are not the right way up.
+3. Run `mkdir -p data/train-v2/raw-new; cp data/train-v2/raw-archive/batch-nnnnn/* data/train-v2/raw-new`
+4. Run `CheckRawTrainingImagesV2`. This will check that the images all have the correct orientation and the
+individual cards can be detected.
+5. If there are problematic images, then copy them to _raw-problem_. These will not be used, but
+keep them as future versions of the code may be able to handle them.
+6. Run `SortRawTrainingImagesV2`. This will programatically detect the number of features on
+each card so that it can sort the training boards with 1, 2, or 3 number cards. (Note 3 is called 0.)
+7. Open each directory in _raw-sorted_ and visually check that each board is in the correct
+directory. Move any that are not.
+8. Run `CreateTrainingSetV2`. This will take each board in _raw-sorted_ and extract labelled
+individual cards and store them in _raw-labelled_, then open a window showing each set of
+cards. Visually inspect these to check they are correct. Move any images that are not.
+9. Run 
+```
+rsync -a data/train-v2/raw-labelled/ data/train-v2/labelled/
+rm -rf data/train-v2/raw-{new,sorted,labelled}
+```
 
--- src/main/java/com/tom_e_white/set_game/preprocess/CreateTrainingSetV2.java
+++ src/main/java/com/tom_e_white/set_game/preprocess/CreateTrainingSetV2.java
@@ -1,12 +1,20 @@
 package com.tom_e_white.set_game.preprocess;
 
+import boofcv.gui.ListDisplayPanel;
+import boofcv.gui.image.ImageGridPanel;
+import boofcv.gui.image.ShowImages;
 import boofcv.io.image.UtilImageIO;
 import com.tom_e_white.set_game.model.Card;
+import net.coobird.thumbnailator.Thumbnails;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static com.tom_e_white.set_game.preprocess.TrainingDataV2.RAW_LABELLED_DIRECTORY;
 import static com.tom_e_white.set_game.preprocess.TrainingDataV2.RAW_SORTED_DIRECTORY;
 
 /**
@@ -14,26 +22,50 @@ import static com.tom_e_white.set_game.preprocess.TrainingDataV2.RAW_SORTED_DIRE
  */
 public class CreateTrainingSetV2 {
     public static void main(String[] args) throws IOException {
-        CardDetector cardDetector = new CardDetector(4, 66);
-        File outDir = TrainingDataV2.RAW_LABELLED_DIRECTORY;
-        outDir.mkdirs();
-        for (File d : RAW_SORTED_DIRECTORY.listFiles((dir, name) -> name.matches("\\d"))) {
-            for (File file : d.listFiles((dir, name) -> name.matches(".*\\.jpg"))) {
-                System.out.println(file);
-                List<CardImage> images = cardDetector.detect(file.getAbsolutePath(), false, true, 3, 9);
-                int i = 0;
-                for (CardImage image : images) {
-                    String numberLabel = file.getParentFile().getName();
-                    Card card = new Card(Integer.valueOf(numberLabel), i / 9, (i % 9) / 3, i % 3);
-                    File labelledDirectory = new File(outDir, card.getDescription().replace(" ", "-"));
-                    labelledDirectory.mkdirs();
-                    File newFile = new File(labelledDirectory, file.getName().replace(".jpg", "_" + i + ".jpg"));
-                    System.out.println(newFile);
-                    UtilImageIO.saveImage(image.getImage(), newFile.getAbsolutePath());
-                    i++;
+        if (!(args.length > 0 && args[0].equals("-viewOnly"))) {
+            CardDetector cardDetector = new CardDetector(4, 66);
+            File outDir = RAW_LABELLED_DIRECTORY;
+            outDir.mkdirs();
+            for (File d : RAW_SORTED_DIRECTORY.listFiles((dir, name) -> name.matches("\\d"))) {
+                for (File file : d.listFiles((dir, name) -> name.matches(".*\\.jpg"))) {
+                    System.out.println(file);
+                    List<CardImage> images = cardDetector.detect(file.getAbsolutePath(), false, true, 3, 9);
+                    int i = 0;
+                    for (CardImage image : images) {
+                        String numberLabel = file.getParentFile().getName();
+                        Card card = new Card(Integer.valueOf(numberLabel), i / 9, (i % 9) / 3, i % 3);
+                        File labelledDirectory = new File(outDir, card.getDescription().replace(" ", "-"));
+                        labelledDirectory.mkdirs();
+                        File newFile = new File(labelledDirectory, file.getName().replace(".jpg", "_" + i + ".jpg"));
+                        System.out.println(newFile);
+                        UtilImageIO.saveImage(image.getImage(), newFile.getAbsolutePath());
+                        i++;
+                    }
                 }
-                break;
             }
+        }
+
+        ListDisplayPanel panel = new ListDisplayPanel();
+        int numCols = 15;
+
+        for (File d : RAW_LABELLED_DIRECTORY.listFiles((dir, name) -> !name.matches("\\..*"))) {
+            String label = d.getName();
+            BufferedImage[] bufferedImages = Arrays.stream(d.listFiles((dir, name) -> name.matches(".*\\.jpg")))
+                    .map(f -> UtilImageIO.loadImage(f.getAbsolutePath()))
+                    .map(bi -> resize(bi, bi.getWidth() / 3, bi.getHeight() / 3))
+                    .collect(Collectors.toList())
+                    .toArray(new BufferedImage[0]);
+            panel.addItem(new ImageGridPanel((bufferedImages.length / numCols) + 1, numCols, bufferedImages), label);
+        }
+
+        ShowImages.showWindow(panel, CreateTrainingSetV2.class.getSimpleName(), true);
+    }
+
+    public static BufferedImage resize(BufferedImage img, int newW, int newH) {
+        try {
+            return Thumbnails.of(img).size(newW, newH).asBufferedImage();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
