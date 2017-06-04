4e232eae44b6d9f52926b2bdda96c36a2e63468e
Tom White <tom@cloudera.com>
2017-06-04 14:06:28 +0100

    Improve PlaySet so it can respond to mouse clicks to get next image

---
+++ src/main/java/com/tom_e_white/set_game/image/ImageSuppliers.java
@@ -0,0 +1,97 @@
+package com.tom_e_white.set_game.image;
+
+import boofcv.io.image.UtilImageIO;
+import com.github.sarxos.webcam.Webcam;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.function.Supplier;
+
+public class ImageSuppliers {
+
+    private static final File WEBCAM_DIR = new File("data/webcam");
+
+
+    public static class ReverseChronologicalSavedImageSupplier implements Supplier<BufferedImage> {
+
+        private final File[] files = listSorted();
+        private int index = files.length - 1;
+
+        static File[] listSorted() {
+            File[] files = WEBCAM_DIR.listFiles();
+            Arrays.sort(files);
+            return files;
+        }
+
+        @Override
+        public BufferedImage get() {
+            if (files.length == 0) {
+                return null;
+            }
+            File file = files[index];
+            index--;
+            if (index < 0) {
+                index = files.length - 1;
+            }
+            System.out.println("Using " + file);
+            return UtilImageIO.loadImage(file.getAbsolutePath());
+        }
+    }
+
+    public static class RandomSavedImageSupplier implements Supplier<BufferedImage> {
+        private final Random r = new Random();
+
+        @Override
+        public BufferedImage get() {
+            File[] files = WEBCAM_DIR.listFiles();
+            if (files.length == 0) {
+                return null;
+            }
+            File file = files[r.nextInt(files.length)];
+            System.out.println("Using " + file);
+            return UtilImageIO.loadImage(file.getAbsolutePath());
+        }
+    }
+
+    public static class NamedSavedImageSupplier implements Supplier<BufferedImage> {
+
+        private String file;
+
+        public NamedSavedImageSupplier(String file) {
+            this.file = file;
+        }
+
+        @Override
+        public BufferedImage get() {
+            return UtilImageIO.loadImage(file);
+        }
+    }
+
+    public static class WebcamImageSupplier implements Supplier<BufferedImage> {
+
+        private Webcam webcam;
+
+        public WebcamImageSupplier(Webcam webcam) {
+            this.webcam = webcam;
+        }
+
+        @Override
+        public BufferedImage get() {
+            BufferedImage image = webcam.getImage();
+            DateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss");
+            File file = new File(WEBCAM_DIR, format.format(new Date()) + ".png");
+            try {
+                ImageIO.write(image, "PNG", file);
+                System.out.println("Saved image to " + file);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return image;
+        }
+    }
+}
--- src/main/java/com/tom_e_white/set_game/predict/PlaySet.java
+++ src/main/java/com/tom_e_white/set_game/predict/PlaySet.java
@@ -1,9 +1,14 @@
 package com.tom_e_white.set_game.predict;
 
-import boofcv.gui.ListDisplayPanel;
 import boofcv.gui.feature.VisualizeShapes;
+import boofcv.gui.image.ImagePanel;
 import boofcv.gui.image.ShowImages;
-import boofcv.io.image.UtilImageIO;
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.ds.ipcam.IpCamDevice;
+import com.github.sarxos.webcam.ds.ipcam.IpCamDeviceRegistry;
+import com.github.sarxos.webcam.ds.ipcam.IpCamDriver;
+import com.github.sarxos.webcam.ds.ipcam.IpCamMode;
+import com.tom_e_white.set_game.image.ImageSuppliers;
 import com.tom_e_white.set_game.model.Card;
 import com.tom_e_white.set_game.model.Cards;
 import com.tom_e_white.set_game.model.Triple;
@@ -11,23 +16,71 @@ import com.tom_e_white.set_game.preprocess.CardDetector;
 import com.tom_e_white.set_game.preprocess.CardImage;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.text.ParseException;
 import java.util.*;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 /**
- * Use {@link CardPredictor} to play Set.
+ * Use {@link CardPredictor} to play Set using a webcam, or another image source, such as a file.
  */
 public class PlaySet {
 
-    public static void play(File testFile) throws IOException, ParseException {
-        CardDetector cardDetector = new CardDetector();
-        CardPredictor cardPredictor = new CardPredictor();
-        List<CardImage> images = cardDetector.detect(testFile.getAbsolutePath(), false);
+    private final Supplier<BufferedImage> imageSupplier;
+    private final ImagePanel panel;
+
+    public PlaySet(Supplier<BufferedImage> imageSupplier, boolean debug) {
+        this.imageSupplier = imageSupplier;
+        this.panel = new ImagePanel(annotateImage(imageSupplier.get(), debug));
+        addMouseListener(panel);
+        ShowImages.showWindow(panel, PlaySet.class.getSimpleName(), true);
+    }
+
+    private void addMouseListener(ImagePanel panel) {
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    System.out.println("Next image");
+                    newImage(imageSupplier.get());
+                }
+            }
+        });
+    }
+
+    private void newImage(BufferedImage image) {
+        panel.setBufferedImage(annotateImage(image, false));
+    }
+
+    private static Webcam findWebcam(String url) throws MalformedURLException {
+        if (url != null) {
+            Webcam.setDriver(new IpCamDriver());
+            IpCamDeviceRegistry.register(new IpCamDevice("Set", url, IpCamMode.PULL));
+        }
+        Webcam webcam = Webcam.getDefault();
+        if (webcam == null) {
+            throw new IllegalStateException("Cannot find webcam");
+        }
+        webcam.open();
+        return webcam;
+    }
+
+    public static BufferedImage annotateImage(BufferedImage image, boolean debug) {
+        CardDetector cardDetector = new CardDetector(4, 66); // TODO: blur radius should be a function of image size
+        CardPredictor cardPredictor = new CardPredictor(2);
+        List<CardImage> images;
+        try {
+            images = cardDetector.detect(image, null, debug, true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         List<Card> cards = images.stream().map(cardImage -> {
             try {
@@ -42,26 +95,45 @@ public class PlaySet {
             cardToImageMap.put(cards.get(i), images.get(i));
         }
 
-        Set<Triple> sets = Cards.sets(cards);
-
-        sets.forEach(System.out::println);
-
-        ListDisplayPanel panel = new ListDisplayPanel();
-        BufferedImage mainImage = UtilImageIO.loadImage(testFile.getAbsolutePath());
-        Graphics2D g2 = mainImage.createGraphics();
+        Graphics2D g2 = image.createGraphics();
         g2.setStroke(new BasicStroke(10));
         g2.setColor(Color.BLUE);
-        Triple set = new ArrayList<>(sets).get(1); // TODO: check if exists
 
-        VisualizeShapes.draw(cardToImageMap.get(set.first()).getExternalQuadrilateral(), g2);
-        VisualizeShapes.draw(cardToImageMap.get(set.second()).getExternalQuadrilateral(), g2);
-        VisualizeShapes.draw(cardToImageMap.get(set.third()).getExternalQuadrilateral(), g2);
+        if (cards.isEmpty()) {
+            System.out.println("No cards found in image");
+        } else {
+            System.out.println(cards);
+            Set<Triple> sets = Cards.sets(cards);
+            sets.forEach(System.out::println);
+            if (sets.isEmpty()) {
+                System.out.println("No sets found");
+            } else {
+                Triple set = new ArrayList<>(sets).get(0);
+                VisualizeShapes.draw(cardToImageMap.get(set.first()).getExternalQuadrilateral(), g2);
+                VisualizeShapes.draw(cardToImageMap.get(set.second()).getExternalQuadrilateral(), g2);
+                VisualizeShapes.draw(cardToImageMap.get(set.third()).getExternalQuadrilateral(), g2);
+            }
+        }
 
-        panel.addImage(mainImage, "Game");
-        ShowImages.showWindow(panel, PlaySet.class.getSimpleName(), true);
+        return image;
     }
 
     public static void main(String[] args) throws Exception {
-        play(new File(args[0]));
+        // "http://192.168.1.92:8080/shot.jpg"
+        boolean debug = false;
+        Supplier<BufferedImage> imageSupplier = new ImageSuppliers.ReverseChronologicalSavedImageSupplier();
+        if (args.length > 0) {
+            if (args[0].equals("webcam")) {
+                String url = args.length == 1 ? null : args[1];
+                imageSupplier = new ImageSuppliers.WebcamImageSupplier(findWebcam(url));
+            } else if (args[0].equals("reverse")) {
+                imageSupplier = new ImageSuppliers.ReverseChronologicalSavedImageSupplier();
+            } else if (args[0].equals("random")) {
+                imageSupplier = new ImageSuppliers.RandomSavedImageSupplier();
+            } else if (args[0].equals("file")) {
+                imageSupplier = new ImageSuppliers.NamedSavedImageSupplier(new File(args[1]).getAbsolutePath());
+            }
+        }
+        new PlaySet(imageSupplier, debug);
     }
 }
