688be2b27e19ff0a44bdd8207781c7027280c30d
Tom White <tom@cloudera.com>
2017-09-14 09:36:48 +0100

    Use previous set for visual continuity

--- src/main/java/com/tom_e_white/set_game/predict/PlaySet.java
+++ src/main/java/com/tom_e_white/set_game/predict/PlaySet.java
@@ -21,6 +21,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,6 +42,7 @@ public class PlaySet implements Runnable{
     private final CardPredictor cardPredictor;
     private final ImagePanel panel;
     private final boolean streaming;
+    private Triple previousSet;
 
     public PlaySet(Supplier<BufferedImage> imageSupplier, boolean streaming, boolean debug) throws IOException {
         this.imageSupplier = imageSupplier;
@@ -101,7 +103,7 @@ public class PlaySet implements Runnable{
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Time to detect cards: " + (System.currentTimeMillis() - start));
+        //System.out.println("Time to detect cards: " + (System.currentTimeMillis() - start));
 
         start = System.currentTimeMillis();
         List<CardPrediction> cardPredictions = images.stream().map(cardImage -> {
@@ -111,7 +113,7 @@ public class PlaySet implements Runnable{
                 throw new RuntimeException(e);
             }
         }).collect(Collectors.toList());
-        System.out.println("Time to predict cards: " + (System.currentTimeMillis() - start));
+        //System.out.println("Time to predict cards: " + (System.currentTimeMillis() - start));
 
         if (cardPredictions.isEmpty()) {
             System.out.println("No cards found in image");
@@ -123,7 +125,6 @@ public class PlaySet implements Runnable{
             if (setPredictions.isEmpty()) {
                 System.out.println("No sets found");
             } else {
-                Triple set = setPredictions.get(0).getSet();
                 Map<Card, CardImage> cardToImageMap = new LinkedHashMap<>();
                 for (int i = 0; i < cardPredictions.size(); i++) {
                     cardToImageMap.put(cardPredictions.get(i).getCard(), images.get(i));
@@ -140,16 +141,25 @@ public class PlaySet implements Runnable{
         Color[] colors = new Color[] {
                 Color.BLUE, Color.RED, Color.GREEN
         };
-        float topProbability = setPredictions.get(0).getProbability();
-        for (int i = 0; i < setPredictions.size(); i++) {
+        List<Triple> sets = setPredictions.stream().map(SetPrediction::getSet).collect(Collectors.toList());
+        List<Triple> setsToShow = new ArrayList<>();
+        if (maxToShow == 1 && sets.contains(previousSet)) { // use previous for visual continuity
+            setsToShow.add(previousSet);
+        } else {
+            float topProbability = setPredictions.get(0).getProbability();
+            previousSet = setPredictions.get(0).getSet();
+            for (SetPrediction setPrediction : setPredictions) {
+                if (setPrediction.getProbability() < topProbability) {
+                    break;
+                }
+                setsToShow.add(setPrediction.getSet());
+            }
+        }
+        for (int i = 0; i < setsToShow.size(); i++) {
             if (i == maxToShow) {
                 break;
             }
-            SetPrediction setPrediction = setPredictions.get(i);
-            if (setPrediction.getProbability() < topProbability) {
-                break;
-            }
-            Triple set = setPrediction.getSet();
+            Triple set = setsToShow.get(i);
             g2.setStroke(new BasicStroke(8 - (i * 2)));
             g2.setColor(colors[i % colors.length]);
             VisualizeShapes.draw(cardToImageMap.get(set.first()).getExternalQuadrilateral(), g2);
